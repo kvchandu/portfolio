0c69c8bbe6b39555e4f71e31247a6852c0a7b6bc
kvchandu <kchandu@ucsd.edu>
2023-07-09 18:12:45 -0700

    Header Section and Navbar added

---
+++ portfolio-website/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import NavBar from "./Components/NavBar";
+import HeaderSection from "./Components/HeaderSection";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <NavBar />
+      <HeaderSection />
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio-website/src/Components/HeaderSection.tsx
@@ -0,0 +1,20 @@
+function HeaderSection() {
+  return (
+    <div className="HeaderSection mt-5">
+      <h1 className="text-center"> Krishna Vamsi Chandu</h1>
+      <ul className="list-group list-group-horizontal justify-content-center border-left-0">
+        <li className="list-group-item border-top-0 border-bottom-0 border-left-0">
+          kchandu@ucsd.edu
+        </li>
+        <li className="list-group-item border-top-0 border-bottom-0">
+          224-714-8908
+        </li>
+        <li className="list-group-item border-top-0 border-bottom-0">
+          linkedin.com/kchand23
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default HeaderSection;
