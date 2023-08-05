128f2161f24e5e614ffe260e4d34c4690ed5ed45
kvchandu <kchandu@ucsd.edu>
2023-08-05 08:56:45 -0700

    Heaeder section modified

--- portfolio-website/src/Components/HeaderSection.tsx
+++ portfolio-website/src/Components/HeaderSection.tsx
@@ -1,18 +1,39 @@
+import React from "react";
+
 function HeaderSection() {
   return (
-    <div className="HeaderSection mt-5">
-      <h1 className="text-center"> Krishna Vamsi Chandu</h1>
-      <ul className="list-group list-group-horizontal justify-content-center border-left-0">
-        <li className="list-group-item border-top-0 border-bottom-0 border-left-0">
-          kchandu@ucsd.edu
-        </li>
-        <li className="list-group-item border-top-0 border-bottom-0">
-          224-714-8908
-        </li>
-        <li className="list-group-item border-top-0 border-bottom-0">
-          linkedin.com/kchand23
-        </li>
-      </ul>
+    <div className="HeaderSection mt-5 container-fluid">
+      <div className="row d-flex align-items-center">
+        <div className="col-3 text-right">
+          <img
+            src="src/assets/portrait.png"
+            alt="Your Name"
+            className="img-fluid profile-img"
+          />
+        </div>
+        <div className="col-6 text-center">
+          <h1 className="header-name">Krishna Vamsi Chandu</h1>
+        </div>
+        <div className="col-3 text-left">
+          <ul className="list-group">
+            <li className="list-group-item border-top-0 border-bottom-0 border-left-0">
+              kchandu@ucsd.edu
+            </li>
+            <li className="list-group-item border-top-0 border-bottom-0">
+              224-714-8908
+            </li>
+            <li className="list-group-item border-top-0 border-bottom-0">
+              <a
+                href="https://linkedin.com/kchand23"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                linkedin.com/kchand23
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
--- portfolio-website/src/Components/NavBar.tsx
+++ portfolio-website/src/Components/NavBar.tsx
@@ -16,20 +16,10 @@ function NavBar() {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="#">
-                Home
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-                Link
-              </a>
-            </li>
-          </ul>
-        </div>
+        <div
+          className="collapse navbar-collapse"
+          id="navbarSupportedContent"
+        ></div>
       </div>
     </nav>
   );
