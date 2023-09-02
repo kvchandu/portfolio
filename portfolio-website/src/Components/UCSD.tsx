bf0a01837de90376b31a6fd439b1caa2aba5e1da
kvchandu <kchandu@ucsd.edu>
2023-09-01 17:35:09 -0700

    Two column Structure added ot the site

---
+++ portfolio-website/src/Components/UCSD.tsx
@@ -0,0 +1,5 @@
+function UCSD() {
+  return <div>Master of Science in Computer Science</div>;
+}
+
+export default UCSD;
---
+++ portfolio-website/src/Components/UIC.tsx
@@ -0,0 +1,5 @@
+function UIC() {
+  return <div>Bachelor of Science in Computer Science</div>;
+}
+
+export default UIC;
