cd9d9108ffc3bfd9716e565d8c569a41a28ff792
kvchandu <kchandu@ucsd.edu>
2023-09-28 18:31:59 -0700

    Header animations added

--- portfolio-website/src/Components/HeaderSection.tsx
+++ portfolio-website/src/Components/HeaderSection.tsx
@@ -13,26 +13,22 @@ function HeaderSection() {
         </div>
         <div className="col-7 text-center">
           <h1 className="header-name">Krishna Vamsi Chandu</h1>
-          <p className="header-content">
-            Welcome to my portfolio! With over 3 years of experience in Full
-            Stack Development, I specialize in crafting software that's not just
-            functional but also scales seamlessly, prioritizes security, and
-            operates with efficiency.{" "}
-          </p>
-          <p className="header-content">
-            I have a passion for building projects that leverage a variety of
-            frameworks, tools, and AI advancements, allowing me to explore my
-            creativity while delivering practical solutions. Beyond coding, I'm
-            an enthusiastic researcher in Computer Vision and Machine Learning.
-            My dedication lies in applying the latest developments in these
-            domains to address real-world challenges.
-          </p>
-          <p className="header-content">
-            {" "}
-            Let's connect and journey together through the exciting realms of
-            software and innovation. Feel free to get in touch using the contact
-            information below.
-          </p>
+          <div className="wrapper">
+            <ul className="dynamic-text">
+              <li>
+                <span>Machine Learning Engineer</span>
+              </li>
+              <li>
+                <span> Full Stack Developer</span>
+              </li>
+              <li>
+                <span>Student Researcher</span>
+              </li>
+              <li>
+                <span>User Interface Designer</span>
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="col-2 text-left">
           <ul className="list-group contact-info">
