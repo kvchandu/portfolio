3fc58f94325b768cd9a866cce8f3733a6c860221
kvchandu <kchandu@ucsd.edu>
2023-09-28 19:03:20 -0700

    Header body added

--- portfolio-website/src/Components/HeaderSection.tsx
+++ portfolio-website/src/Components/HeaderSection.tsx
@@ -30,6 +30,7 @@ function HeaderSection() {
             </ul>
           </div>
         </div>
+
         <div className="col-2 text-left">
           <ul className="list-group contact-info">
             <li className="list-group-item border-top-0 border-bottom-0 border-left-0">
@@ -49,6 +50,28 @@ function HeaderSection() {
             </li>
           </ul>
         </div>
+      </div>
+      <div className="row justify-content-center header-content-wrapper align-items-center flex-grow-1">
+        <p className="header-content">
+          Welcome to my portfolio! With over 3 years of experience in Full Stack
+          Development, I specialize in crafting software that's not just
+          functional but also scales seamlessly, prioritizes security, and
+          operates with efficiency.{" "}
+        </p>
+        <p className="header-content">
+          I have a passion for building projects that leverage a variety of
+          frameworks, tools, and AI advancements, allowing me to explore my
+          creativity while delivering practical solutions. Beyond coding, I'm an
+          enthusiastic researcher in Computer Vision and Machine Learning. My
+          dedication lies in applying the latest developments in these domains
+          to address real-world challenges.
+        </p>
+        <p className="header-content">
+          {" "}
+          Let's connect and journey together through the exciting realms of
+          software and innovation. Feel free to get in touch using the contact
+          information below.
+        </p>
       </div>
     </div>
   );
